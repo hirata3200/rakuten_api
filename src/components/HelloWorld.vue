34c8f388b086011ddc4486cf6ac7d04807debd10
hirata3200 <k1420kei@gmail.com>
2021-10-27 13:02:43 +0900

    vue-api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,76 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <!-- <v-btn v-on:click="gatData">ボタン</v-btn> -->
+    <table>
+      <tr>
+        <td class="title">
+          <h4>タイトル</h4><h2>{{ title }}</h2>
+        </td>
+        <td class="author">
+          <h5>著者/イラスト</h5><h2>{{ author }}</h2>
+        </td>
+      </tr>
+      <tr>
+        <td class="itemcaption">
+          <h4 style="text-align:center">あらすじ</h4><h3>{{ itemCaption }}</h3>
+        </td>
+        <td class="seriesnamekana">
+          <h4>出版社</h4><h3>{{ seriesNameKana }}</h3>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      title: "",
+      author: "",
+      itemCaption: "",
+      seriesNameKana: "",
+    };
+  },
+  async mounted() {
+    const url =
+      "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&title=%E3%82%BC%E3%83%AD%E3%81%8B%E3%82%89%E5%A7%8B%E3%82%81%E3%82%8B%E7%95%B0%E4%B8%96%E7%95%8C%E7%94%9F%E6%B4%BB%EF%BC%91&applicationId=1065554492451182173";
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.log(data.Items[10]);
+    this.title = data.Items[0].Item.title;
+    this.author = data.Items[0].Item.author;
+    this.itemCaption = data.Items[0].Item.itemCaption;
+    this.seriesNameKana = data.Items[0].Item.seriesNameKana;
+  },
+};
 </script>
+<style>
+.title {
+  width: 250px;
+  border: solid 5px blue;
+  text-align: center;
+  color: white;
+  background-color: green;
+}
+.author {
+  width: 130px;
+  border: solid 5px red;
+  text-align: center;
+    color: white;
+  background-color: green;
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.itemcaption {
+  width: 1000px;
+  border: solid 2px red;
+  text-align: left;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.seriesnamekana {
+  width: 130px;
+  border: solid 2px green;
+  text-align: center;
+
 }
 </style>
