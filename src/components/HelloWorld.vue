5a4d2d1b69bd0e5b97a78af9da4f0d117b13dc9f
hirata3200 <k1420kei@gmail.com>
2021-10-28 10:21:58 +0900

    演習

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
-    <!-- <v-btn v-on:click="gatData">ボタン</v-btn> -->
+    <input class="form-control" type="text" v-model="input">
+    <button class="btn btn-info mt-2" v-on:click="doAction">検索</button>
     <table>
       <tr>
         <td class="title">
           <h4>タイトル</h4><h2>{{ title }}</h2>
         </td>
-        <td class="author">
+        <!-- <td class="author">
           <h5>著者/イラスト</h5><h2>{{ author }}</h2>
         </td>
       </tr>
@@ -16,7 +17,7 @@
         </td>
         <td class="seriesnamekana">
           <h4>出版社</h4><h3>{{ seriesNameKana }}</h3>
-        </td>
+        </td> -->
       </tr>
     </table>
   </div>
@@ -31,18 +32,20 @@ export default {
       author: "",
       itemCaption: "",
       seriesNameKana: "",
-    };
+    }
   },
-  async mounted() {
+  async methods: {
+    doAction(){
     const url =
       "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&title=%E3%82%BC%E3%83%AD%E3%81%8B%E3%82%89%E5%A7%8B%E3%82%81%E3%82%8B%E7%95%B0%E4%B8%96%E7%95%8C%E7%94%9F%E6%B4%BB%EF%BC%91&applicationId=1065554492451182173";
     const response = await fetch(url);
     const data = await response.json();
-    // console.log(data.Items[10]);
     this.title = data.Items[0].Item.title;
-    this.author = data.Items[0].Item.author;
-    this.itemCaption = data.Items[0].Item.itemCaption;
-    this.seriesNameKana = data.Items[0].Item.seriesNameKana;
+    // this.author = data.Items[0].Item.author;
+    // this.itemCaption = data.Items[0].Item.itemCaption;
+    // this.seriesNameKana = data.Items[0].Item.seriesNameKana;
+
+    //v-onのページ見てやること
   },
 };
 </script>
